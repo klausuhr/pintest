3819849876aa20cfe5015f9fc31e3c40079062b1
Klaus Uhr <klausuhr@gmail.com>
2013-02-27 12:29:27 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pintest::Application.config.secret_token = '8424f035ad1c245e6916d8376e01a1594d198ff44de0ffef1a5cb35a3e66843ae1584daf0a9bae6b49a85d62fa1d808a72f543c8b2ce550fb7263cd85629bf36'
